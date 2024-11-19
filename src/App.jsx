d9a0e739b2afa7dfcda8c55c4023d5549e7fbed8
anhnguyen6535 <anhnguyen6535@gmail.com>
2024-11-18 23:55:55 -0700

    add sound for demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 // import FaceDetection from './Components/FaceDetection';
 import SpatialAwareness from './Components/SpatialAwareness';
 
 const App = () => {
+  const audioRef = useRef()
+
+  const playAudio = () =>{
+    if(audioRef.current && audioRef.current.paused){
+      audioRef.current.play()
+    }
+  }
+
+  useEffect(() =>{
+    document.addEventListener('click', playAudio, {once: true})
+
+    return () =>{
+      document.removeEventListener('click', playAudio)
+    }
+  },[])
+
   return<>
     {/* <FaceDetection /> */}
     <SpatialAwareness />
+    <audio ref={audioRef} src="/audio.mp3" loop/>
   </>
 };
 
